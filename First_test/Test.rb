92d706ed6e1cc5f9cf22409678b6597d1614525e
Maksym Tsapura <maks0978027942@gmail.com>
2022-11-16 16:02:49 +0200

    try click button

--- First_test/Test.rb
+++ First_test/Test.rb
@@ -1,5 +1,22 @@
 require 'selenium-webdriver'
+require 'test-unit'
 
-driver = Selenium::WebDriver.for:chrome
+class GoogleTest < Test::Unit::TestCase
 
-driver.get 'https://rozetka.com.ua/' 
+    def setup
+        @driver = Selenium::WebDriver.for :chrome
+        @url = 'https://rozetka.com.ua/'
+        '''action = Selenium::WebDriver::ActionBuilder'''
+        @driver.get(@url)
+        @driver.manage.window.maximize
+        @driver.manage.timeouts.implicit_wait = 30
+        @wait = Selenium::WebDriver::Wait.new(:timeout => 30)
+        
+    end
+
+    def test_button
+        @button_box = @driver.find_element(:class_name, "header-actions__component")
+        @button_box.click
+    end
+
+end
