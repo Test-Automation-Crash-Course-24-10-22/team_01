980753d63f67c72a592d4bdc771a173af5701250
Maksym Tsapura <maks0978027942@gmail.com>
2022-11-20 14:57:48 +0200

    delete @

--- First_test/Test.rb
+++ First_test/Test.rb
@@ -4,18 +4,14 @@ require 'test-unit'
 class GoogleTest < Test::Unit::TestCase
 
     def setup
-        @driver = Selenium::WebDriver.for :chrome
-        @url = 'https://rozetka.com.ua/'
-        '''action = Selenium::WebDriver::ActionBuilder'''
-        @driver.get(@url)
-        @driver.manage.window.maximize
-        @driver.manage.timeouts.implicit_wait = 30
-        @wait = Selenium::WebDriver::Wait.new(:timeout => 30)
-        
+        driver = Selenium::WebDriver.for :chrome
+        url = 'https://rozetka.com.ua/'
+        driver.get(url)
+        driver.manage.window.maximize        
     end
 
     def test_button
-        @button_box = @driver.find_element(:class_name, "header-actions__component")
-        @button_box.click
+        button = driver.find_element(:class_name, "header-actions__component")
+        button.click
     end
 end
