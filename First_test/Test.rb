3adf672745a1c6ded2c21bd95fc265dc6118f49a
Maksym Tsapura <maks0978027942@gmail.com>
2022-11-21 13:17:46 +0200

    find_element use xpath

--- First_test/Test.rb
+++ First_test/Test.rb
@@ -2,7 +2,10 @@ require 'selenium-webdriver'
 require 'test-unit'
 
 driver = Selenium::WebDriver.for :chrome
-    
-driver.get 'https://rozetka.com.ua/'
+url = "https://rozetka.com.ua/promo/black-friday/"
 
-driver.quit
+driver.get(url)
+button_to_loggin_form = driver.find_element(:xpath, "/html/body/app-root/div/div/rz-header/rz-main-header/header/div/div/ul/li[3]/rz-user/button").click()
+loggin_form = driver.find_element(:xpath, "/html/body/app-root/rz-single-modal-window/div[3]/div[2]/rz-user-identification/rz-auth/div/form/fieldset/div[1]/input").send_keys("automationtestrozetka@gmail.com")
+password_form = driver.find_element(:xpath, "/html/body/app-root/rz-single-modal-window/div[3]/div[2]/rz-user-identification/rz-auth/div/form/fieldset/div[2]/div/div/input").send_keys("qseqLAS3IT8w2")
+button_to_loggin = driver.find_element(:xpath, "/html/body/app-root/rz-single-modal-window/div[3]/div[2]/rz-user-identification/rz-auth/div/form/fieldset/div[5]/button[1]").click()
