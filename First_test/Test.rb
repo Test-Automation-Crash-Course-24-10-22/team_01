b64c9561429457fc9f8d0123d6f55a753377c218
Maksym Tsapura <maks0978027942@gmail.com>
2022-11-21 14:11:00 +0200

    test loggining

--- First_test/Test.rb
+++ First_test/Test.rb
@@ -6,6 +6,7 @@ url = "https://rozetka.com.ua/promo/black-friday/"
 
 driver.get(url)
 button_to_loggin_form = driver.find_element(:xpath, "/html/body/app-root/div/div/rz-header/rz-main-header/header/div/div/ul/li[3]/rz-user/button").click()
-loggin_form = driver.find_element(:xpath, "/html/body/app-root/rz-single-modal-window/div[3]/div[2]/rz-user-identification/rz-auth/div/form/fieldset/div[1]/input").send_keys("automationtestrozetka@gmail.com")
-password_form = driver.find_element(:xpath, "/html/body/app-root/rz-single-modal-window/div[3]/div[2]/rz-user-identification/rz-auth/div/form/fieldset/div[2]/div/div/input").send_keys("qseqLAS3IT8w2")
+driver.manage.timeouts.implicit_wait=10
+loggin_form = driver.find_element(:id, "auth_email").send_keys("automationtestrozetka@gmail.com")
+password_form = driver.find_element(:id, "auth_pass").send_keys("qseqLAS3IT8w2")
 button_to_loggin = driver.find_element(:xpath, "/html/body/app-root/rz-single-modal-window/div[3]/div[2]/rz-user-identification/rz-auth/div/form/fieldset/div[5]/button[1]").click()
