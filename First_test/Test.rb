d17d8f3d0b4f3203c3438a9e861b7fd4849791d9
Maksym Tsapura <maks0978027942@gmail.com>
2022-11-21 11:20:50 +0200

    add pom.xml and requirements.txt

--- First_test/Test.rb
+++ First_test/Test.rb
@@ -1,17 +1,8 @@
 require 'selenium-webdriver'
 require 'test-unit'
 
-class GoogleTest < Test::Unit::TestCase
+driver = Selenium::WebDriver.for :chrome
+    
+driver.get 'https://rozetka.com.ua/'
 
-    def setup
-        driver = Selenium::WebDriver.for :chrome
-        url = 'https://rozetka.com.ua/'
-        driver.get(url)
-        driver.manage.window.maximize        
-    end
-
-    def test_button
-        button = driver.find_element(:class_name, "header-actions__component")
-        button.click
-    end
-end
+driver.quit
